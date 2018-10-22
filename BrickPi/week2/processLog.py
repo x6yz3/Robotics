c817ec4c747b0eac486bc416cd23129665571441
chendingyan <dingyan.chen96@gmail.com>
2018-10-22 11:02:31 +0100

    Add more functions: 1. rotate wheel according to the angle in degree 2. move forward with some speed and distance 3. move in a square 4. process logfile such that it will start at 0 5. draw figure for logfile data Ready to tuning our PID! ^-^

--- BrickPi/week2/processLog.py
+++ BrickPi/week2/processLog.py
@@ -21,27 +21,30 @@ def drawfigure(logfile):
     fig = plt.figure(1)
 
     figure1 = plt.subplot(211)
-    plt.plot(data[0], data[1], label="Reference angle")
-    plt.plot(data[0],data[2], label="Actual angle")
+    ref0, = plt.plot(data[0], data[1], label="Reference angle")
+    act0, = plt.plot(data[0],data[2], label="Actual angle")
     figure1.set_title('Left motor', fontsize=14)
     plt.xlabel('Time', fontsize=10)
     plt.ylabel('Angle [Radians]', fontsize=10)
-
+    plt.legend([ref0, act0], ['Reference', 'Actual'])
+    plt.tight_layout()
     figure2 = plt.subplot(212)
-    plt.plot(data[0],data[3], label ="Reference angle")
-    plt.plot(data[0],data[4], label="Actual angle")
+    ref1, = plt.plot(data[0],data[3], label ="Reference angle")
+    act1, = plt.plot(data[0],data[4], label="Actual angle")
     figure2.set_title('Right motor', fontsize=14)
     plt.xlabel('Time', fontsize=10)
     plt.ylabel('Angle [Radians]', fontsize=10)
+    plt.legend([ref1, act1], ['Reference', 'Actual'])
+    plt.tight_layout()
     fig = plt.gcf()
-    # plt.show()
+    #plt.show()
     name = logfile[:-4]
     fig.savefig('figures/'+ name + '.png', dpi = 100)
 
 if __name__ == '__main__':
-    with open('logs/logs_name.txt', 'r') as f:
-        for name in f:
-            processLog('logs/'+ name+'.log')
-            drawfigure('logs/'+ name+'.log')
+    # with open('logs/logs_name.txt', 'r') as f:
+    #     for name in f:
+    #         processLog('logs/'+ name+'.log')
+    #         drawfigure('logs/'+ name+'.log')
     # processLog("logs/motor_position_1_360.0.log")
-    # drawfigure('write.txt')
+    drawfigure('write.txt')
