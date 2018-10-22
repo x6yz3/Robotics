0f397357f877a431b733354ba0e603558a8b2337
chendingyan <dingyan.chen96@gmail.com>
2018-10-22 11:46:33 +0100

    Add more functions: 1. rotate wheel according to the angle in degree 2. move forward with some speed and distance 3. move in a square 4. process logfile such that it will start at 0 5. draw figure for logfile data Ready to tuning our PID! ^-^

--- BrickPi/week2/motor_position_example.py
+++ BrickPi/week2/motor_position_example.py
@@ -12,9 +12,10 @@ kp_max = float(input("Enter maximum Kp:"))
 increment = float(input("Enter incremental value:"))
 
 for i in range(kp_min, kp_max, increment):
-	input_angle = float(input("Enter a angle to rotate (in radians): "))
-	angle = math.radians(input_angle)
-	Robot.wheel_rotate(angle, i)
+    Robot.motorParams["Left"].pidParameters.k_p = i
+    input_angle = float(input("Enter a angle to rotate (in radians): "))
+    angle = math.radians(input_angle)
+    Robot.wheel_rotate(angle, i)
 
 
 interface.terminate()
